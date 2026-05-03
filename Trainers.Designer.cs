caf9dd2fbd8379556c6469aa346d6d61fa26edf4
Karen Nasser <karennasser997@gmail.com>
2026-05-03 19:24:46 +0300

    Add files via upload

--- Trainers.Designer.cs
+++ Trainers.Designer.cs
@@ -23,7 +23,7 @@ namespace Gym_Management
 
         private void InitializeComponent()
         {
-            // init controls
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Trainers));
             label2 = new Label();
             txtFname = new TextBox();
             label3 = new Label();
@@ -46,145 +46,171 @@ namespace Gym_Management
             Delete_Trainer = new Button();
             Search = new Button();
             Back = new Button();
-
+            pictureBox1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-
+            // 
             // label2
+            // 
             label2.Anchor = AnchorStyles.None;
             label2.AutoSize = true;
             label2.Font = new Font("Calibri", 10.2F, FontStyle.Italic);
             label2.Location = new Point(71, 204);
             label2.Name = "label2";
+            label2.Size = new Size(86, 21);
             label2.TabIndex = 1;
             label2.Text = "First Name";
-
+            // 
             // txtFname
+            // 
             txtFname.Anchor = AnchorStyles.None;
             txtFname.Location = new Point(177, 201);
             txtFname.Name = "txtFname";
             txtFname.Size = new Size(222, 27);
             txtFname.TabIndex = 2;
-
+            // 
             // label3
+            // 
             label3.Anchor = AnchorStyles.None;
             label3.AutoSize = true;
             label3.Font = new Font("Calibri", 10.2F, FontStyle.Italic);
             label3.Location = new Point(78, 258);
             label3.Name = "label3";
+            label3.Size = new Size(83, 21);
             label3.TabIndex = 3;
             label3.Text = "Last Name";
-
+            // 
             // txtLname
+            // 
             txtLname.Anchor = AnchorStyles.None;
             txtLname.Location = new Point(177, 255);
             txtLname.Name = "txtLname";
             txtLname.Size = new Size(222, 27);
             txtLname.TabIndex = 4;
-
+            // 
             // label5
+            // 
             label5.Anchor = AnchorStyles.None;
             label5.AutoSize = true;
             label5.Font = new Font("Calibri", 10.2F, FontStyle.Italic);
             label5.Location = new Point(91, 306);
             label5.Name = "label5";
+            label5.Size = new Size(61, 21);
             label5.TabIndex = 5;
             label5.Text = "Gender";
-
+            // 
             // comboBoxGender
+            // 
             comboBoxGender.Anchor = AnchorStyles.None;
             comboBoxGender.FormattingEnabled = true;
             comboBoxGender.Location = new Point(177, 303);
             comboBoxGender.Name = "comboBoxGender";
             comboBoxGender.Size = new Size(222, 28);
             comboBoxGender.TabIndex = 6;
-
+            // 
             // label6
+            // 
             label6.Anchor = AnchorStyles.None;
             label6.AutoSize = true;
             label6.Font = new Font("Calibri", 10.2F, FontStyle.Italic);
             label6.Location = new Point(101, 353);
             label6.Name = "label6";
+            label6.Size = new Size(41, 21);
             label6.TabIndex = 7;
             label6.Text = "Shift";
-
+            // 
             // comboBoxShift
+            // 
             comboBoxShift.Anchor = AnchorStyles.None;
             comboBoxShift.FormattingEnabled = true;
             comboBoxShift.Location = new Point(177, 350);
             comboBoxShift.Name = "comboBoxShift";
             comboBoxShift.Size = new Size(222, 28);
             comboBoxShift.TabIndex = 8;
-
+            // 
             // label7
+            // 
             label7.Anchor = AnchorStyles.None;
             label7.AutoSize = true;
             label7.Font = new Font("Calibri", 10.2F, FontStyle.Italic);
             label7.Location = new Point(71, 403);
             label7.Name = "label7";
+            label7.Size = new Size(76, 21);
             label7.TabIndex = 9;
             label7.Text = "Speciality";
-
+            // 
             // comboBoxSpeciality
+            // 
             comboBoxSpeciality.Anchor = AnchorStyles.None;
             comboBoxSpeciality.FormattingEnabled = true;
             comboBoxSpeciality.Location = new Point(177, 400);
             comboBoxSpeciality.Name = "comboBoxSpeciality";
             comboBoxSpeciality.Size = new Size(222, 28);
             comboBoxSpeciality.TabIndex = 10;
-
+            // 
             // label9
+            // 
             label9.Anchor = AnchorStyles.None;
             label9.AutoSize = true;
             label9.Font = new Font("Calibri", 10.2F, FontStyle.Italic);
             label9.Location = new Point(91, 453);
             label9.Name = "label9";
+            label9.Size = new Size(58, 21);
             label9.TabIndex = 11;
             label9.Text = "Branch";
-
+            // 
             // comboBoxBranch
+            // 
             comboBoxBranch.Anchor = AnchorStyles.None;
             comboBoxBranch.FormattingEnabled = true;
             comboBoxBranch.Location = new Point(177, 450);
             comboBoxBranch.Name = "comboBoxBranch";
             comboBoxBranch.Size = new Size(222, 28);
             comboBoxBranch.TabIndex = 12;
-
+            // 
             // label11
+            // 
             label11.Anchor = AnchorStyles.None;
             label11.AutoSize = true;
             label11.Font = new Font("Calibri", 10.2F, FontStyle.Italic);
             label11.Location = new Point(98, 503);
             label11.Name = "label11";
+            label11.Size = new Size(54, 21);
             label11.TabIndex = 13;
             label11.Text = "Phone";
-
+            // 
             // txtPhone
+            // 
             txtPhone.Anchor = AnchorStyles.None;
             txtPhone.BackColor = SystemColors.Window;
             txtPhone.Location = new Point(177, 500);
             txtPhone.Name = "txtPhone";
             txtPhone.Size = new Size(222, 27);
             txtPhone.TabIndex = 14;
-
+            // 
             // label1
+            // 
             label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
             label1.Font = new Font("Calibri", 10.2F, FontStyle.Italic);
             label1.Location = new Point(117, 558);
             label1.Name = "label1";
+            label1.Size = new Size(24, 21);
             label1.TabIndex = 15;
             label1.Text = "ID";
-
+            // 
             // txtID
+            // 
             txtID.Anchor = AnchorStyles.None;
             txtID.BackColor = SystemColors.Window;
             txtID.Location = new Point(177, 555);
             txtID.Name = "txtID";
             txtID.Size = new Size(222, 27);
             txtID.TabIndex = 16;
-
-            // datagrid setup
+            // 
+            // dataGridView1
+            // 
             dataGridView1.Anchor = AnchorStyles.None;
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.BackgroundColor = Color.White;
@@ -197,8 +223,9 @@ namespace Gym_Management
             dataGridView1.Size = new Size(855, 220);
             dataGridView1.TabIndex = 17;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
-
-            // action buttons
+            // 
+            // Add_Trainer
+            // 
             Add_Trainer.Anchor = AnchorStyles.None;
             Add_Trainer.Location = new Point(557, 330);
             Add_Trainer.Name = "Add_Trainer";
@@ -207,16 +234,9 @@ namespace Gym_Management
             Add_Trainer.Text = "Add Trainer";
             Add_Trainer.UseVisualStyleBackColor = true;
             Add_Trainer.Click += Add_Trainer_Click;
-
-            Search.Anchor = AnchorStyles.None;
-            Search.Location = new Point(557, 375);
-            Search.Name = "Search";
-            Search.Size = new Size(170, 34);
-            Search.TabIndex = 19;
-            Search.Text = "Search For Trainer";
-            Search.UseVisualStyleBackColor = true;
-            Search.Click += Search_Click;
-
+            // 
+            // Update_Trainer
+            // 
             Update_Trainer.Anchor = AnchorStyles.None;
             Update_Trainer.Location = new Point(557, 420);
             Update_Trainer.Name = "Update_Trainer";
@@ -225,7 +245,9 @@ namespace Gym_Management
             Update_Trainer.Text = "Update Trainer";
             Update_Trainer.UseVisualStyleBackColor = true;
             Update_Trainer.Click += Update_Trainer_Click;
-
+            // 
+            // Delete_Trainer
+            // 
             Delete_Trainer.Anchor = AnchorStyles.None;
             Delete_Trainer.Location = new Point(557, 465);
             Delete_Trainer.Name = "Delete_Trainer";
@@ -234,7 +256,20 @@ namespace Gym_Management
             Delete_Trainer.Text = "Delete Trainer";
             Delete_Trainer.UseVisualStyleBackColor = true;
             Delete_Trainer.Click += Delete_Trainer_Click;
-
+            // 
+            // Search
+            // 
+            Search.Anchor = AnchorStyles.None;
+            Search.Location = new Point(557, 375);
+            Search.Name = "Search";
+            Search.Size = new Size(170, 34);
+            Search.TabIndex = 19;
+            Search.Text = "Search For Trainer";
+            Search.UseVisualStyleBackColor = true;
+            Search.Click += Search_Click;
+            // 
+            // Back
+            // 
             Back.Anchor = AnchorStyles.None;
             Back.BackColor = Color.DarkTurquoise;
             Back.Location = new Point(700, 12);
@@ -244,18 +279,25 @@ namespace Gym_Management
             Back.Text = "Back";
             Back.UseVisualStyleBackColor = false;
             Back.Click += Back_Click;
-
-            // main form properties
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(308, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(190, 180);
+            pictureBox1.TabIndex = 23;
+            pictureBox1.TabStop = false;
+            // 
+            // Trainers
+            // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(855, 855);
-            StartPosition = FormStartPosition.CenterScreen;
-            Name = "Trainers";
-            Text = "Trainers Management";
-            Load += Trainers_Load;
-
-            // add elements to form
+            Controls.Add(pictureBox1);
             Controls.Add(Back);
             Controls.Add(dataGridView1);
             Controls.Add(txtID);
@@ -278,8 +320,12 @@ namespace Gym_Management
             Controls.Add(Search);
             Controls.Add(Update_Trainer);
             Controls.Add(Delete_Trainer);
-
+            Name = "Trainers";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Trainers Management";
+            Load += Trainers_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -308,5 +354,6 @@ namespace Gym_Management
         private Button Delete_Trainer;
         private Button Search;
         private Button Back;
+        private PictureBox pictureBox1;
     }
 }

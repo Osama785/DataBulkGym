31ac75fca160efc875e25abba5f2523f0bafa94c
Lobna Ahmed <lopnaahmed015@gmail.com>
2026-05-05 20:50:58 +0300

    Updated some logic related to Branch information

--- Gym.sql
+++ Gym.sql
@@ -4,15 +4,30 @@ DROP TABLE IF EXISTS Class;
 DROP TABLE IF EXISTS Member_Phone;
 DROP TABLE IF EXISTS Member;
 DROP TABLE IF EXISTS Workout_Plan;
-DROP TABLE IF EXISTS Trainer;
-DROP TABLE IF EXISTS Branch_Manager;
+DROP TABLE IF EXISTS Equipment; 
+DROP TABLE IF EXISTS Trainer;  
+DROP TABLE IF EXISTS Branch_Manager; 
 DROP TABLE IF EXISTS Membership;
-DROP TABLE IF EXISTS Equipment;
+USE master;
+GO
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'GymManagementDB')
+BEGIN
+    ALTER DATABASE GymManagementDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE GymManagementDB;
+END
+GO
+
+CREATE DATABASE GymManagementDB;
+GO
+
+USE GymManagementDB;
+GO
 
 CREATE TABLE Membership (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    Duration VARCHAR(50) NOT NULL,
-    Price DECIMAL(10,2) NOT NULL CHECK (Price > 0)
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Duration VARCHAR(50) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL CHECK (Price > 0)
 );
 
 INSERT INTO Membership (Duration, Price) VALUES
@@ -25,31 +40,36 @@ CREATE TABLE Branch_Manager (
     Type VARCHAR(50) NOT NULL CHECK (Type IN ('Male','Female','Mixed')),
     City VARCHAR(50) NOT NULL,
     Area VARCHAR(50) NOT NULL,
-    Manager_ID INT UNIQUE NOT NULL,
+    Manager_ID INT UNIQUE NULL,
     Manager_Fname VARCHAR(50) NOT NULL,
     Manager_Lname VARCHAR(50) NOT NULL,
     Manager_Phone VARCHAR(20) UNIQUE
 );
+SET IDENTITY_INSERT Branch_Manager ON;
 
 INSERT INTO Branch_Manager
-(Type, City, Area, Manager_ID, Manager_Fname, Manager_Lname, Manager_Phone)
+(Branch_ID, Type, City, Area, Manager_ID, Manager_Fname, Manager_Lname, Manager_Phone)
 VALUES
-('Male', 'Cairo', 'Nasr City', 101, 'Ahmed', 'Ali', '01011111111'),
-('Female', 'Giza', 'Dokki', 102, 'Sara', 'Omar', '01022222222'),
-('Mixed', 'Alex', 'Smouha', 103, 'Mona', 'Hassan', '01033333333');
+(1, 'Male', 'Cairo', 'Nasr City', 1, 'Ahmed', 'Ali', '01011111111'),
+(2, 'Female', 'Giza', 'Dokki', 2, 'Sara', 'Omar', '01022222222'),
+(3, 'Mixed', 'Alex', 'Smouha', 3, 'Mona', 'Hassan', '01033333333');
+
+SET IDENTITY_INSERT Branch_Manager OFF;
+DBCC CHECKIDENT ('Branch_Manager', RESEED, 3);
+GO
 
 CREATE TABLE Trainer (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    Fname VARCHAR(50) NOT NULL,
-    Lname VARCHAR(50) NOT NULL,
-    Gender VARCHAR(10) NOT NULL CHECK (Gender IN ('Male', 'Female')),
-    Shift VARCHAR(20) CHECK (Shift IN ('Morning', 'Evening', 'Night')),
-    Speciality VARCHAR(50) CHECK (Speciality IN 
-    ('Cardio', 'Yoga', 'Strength', 'CrossFit', 'Pilates',
-     'Zumba', 'Bodybuilding', 'Aerobics', 'Powerlifting')),
-    Phone VARCHAR(20) UNIQUE,
-    Branch_ID INT NOT NULL,
-    FOREIGN KEY (Branch_ID) REFERENCES Branch_Manager(Branch_ID)
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Fname VARCHAR(50) NOT NULL,
+    Lname VARCHAR(50) NOT NULL,
+    Gender VARCHAR(10) NOT NULL CHECK (Gender IN ('Male', 'Female')),
+    Shift VARCHAR(20) CHECK (Shift IN ('Morning', 'Evening', 'Night')),
+    Speciality VARCHAR(50) CHECK (Speciality IN 
+    ('Cardio', 'Yoga', 'Strength', 'CrossFit', 'Pilates',
+     'Zumba', 'Bodybuilding', 'Aerobics', 'Powerlifting')),
+    Phone VARCHAR(20) UNIQUE,
+    Branch_ID INT NOT NULL,
+    FOREIGN KEY (Branch_ID) REFERENCES Branch_Manager(Branch_ID)
 );
 
 INSERT INTO Trainer
@@ -60,20 +80,20 @@ VALUES
 ('Omar', 'Khaled', 'Male', 'Morning', 'Strength', '01133333333', 3);
 
 CREATE TABLE Workout_Plan (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL CHECK (Name IN (
-        'Weight Loss',
-        'Muscle Gain',
-        'Fitness Starter',
-        'Bodybuilding',
-        'Cardio Blast',
-        'Fat Burn',
-        'Strength Builder'
-    )),
-    Duration INT NOT NULL CHECK (Duration > 0),
-    Intensity_Level VARCHAR(20) NOT NULL CHECK (Intensity_Level IN ('Low', 'Medium', 'High')),
-    Trainer_ID INT NOT NULL,
-    FOREIGN KEY (Trainer_ID) REFERENCES Trainer(ID)
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL CHECK (Name IN (
+        'Weight Loss',
+        'Muscle Gain',
+        'Fitness Starter',
+        'Bodybuilding',
+        'Cardio Blast',
+        'Fat Burn',
+        'Strength Builder'
+    )),
+    Duration INT NOT NULL CHECK (Duration > 0),
+    Intensity_Level VARCHAR(20) NOT NULL CHECK (Intensity_Level IN ('Low', 'Medium', 'High')),
+    Trainer_ID INT NOT NULL,
+    FOREIGN KEY (Trainer_ID) REFERENCES Trainer(ID)
 );
 
 INSERT INTO Workout_Plan
@@ -84,18 +104,18 @@ VALUES
 ('Fitness Starter', 6, 'Low', 3);
 
 CREATE TABLE Member (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    Fname VARCHAR(50) NOT NULL,
-    Lname VARCHAR(50) NOT NULL,
-    Gender VARCHAR(10) NOT NULL CHECK (Gender IN ('Male', 'Female')),
-    Join_Date DATE NOT NULL,
-    Membership_ID INT NOT NULL,
-    Start_Date DATE NOT NULL,
-    Branch_ID INT NOT NULL,
-    Plan_ID INT,
-    FOREIGN KEY (Membership_ID) REFERENCES Membership(ID),
-    FOREIGN KEY (Branch_ID) REFERENCES Branch_Manager(Branch_ID),
-    FOREIGN KEY (Plan_ID) REFERENCES Workout_Plan(ID)
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Fname VARCHAR(50) NOT NULL,
+    Lname VARCHAR(50) NOT NULL,
+    Gender VARCHAR(10) NOT NULL CHECK (Gender IN ('Male', 'Female')),
+    Join_Date DATE NOT NULL,
+    Membership_ID INT NOT NULL,
+    Start_Date DATE NOT NULL,
+    Branch_ID INT NOT NULL,
+    Plan_ID INT,
+    FOREIGN KEY (Membership_ID) REFERENCES Membership(ID),
+    FOREIGN KEY (Branch_ID) REFERENCES Branch_Manager(Branch_ID),
+    FOREIGN KEY (Plan_ID) REFERENCES Workout_Plan(ID)
 );
 
 INSERT INTO Member
@@ -106,10 +126,10 @@ VALUES
 ('Salma', 'Tarek', 'Female', '2026-04-05', 3, '2026-04-06', 3, 3);
 
 CREATE TABLE Member_Phone (
-    Member_ID INT NOT NULL,
-    Phone VARCHAR(20) NOT NULL,
-    PRIMARY KEY (Member_ID, Phone),
-    FOREIGN KEY (Member_ID) REFERENCES Member(ID)
+    Member_ID INT NOT NULL,
+    Phone VARCHAR(20) NOT NULL,
+    PRIMARY KEY (Member_ID, Phone),
+    FOREIGN KEY (Member_ID) REFERENCES Member(ID)
 );
 
 INSERT INTO Member_Phone VALUES
@@ -118,10 +138,10 @@ INSERT INTO Member_Phone VALUES
 (3, '01233333333');
 
 CREATE TABLE Class (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL,
-    Trainer_ID INT NOT NULL,
-    FOREIGN KEY (Trainer_ID) REFERENCES Trainer(ID)
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL,
+    Trainer_ID INT NOT NULL,
+    FOREIGN KEY (Trainer_ID) REFERENCES Trainer(ID)
 );
 
 INSERT INTO Class
@@ -132,12 +152,12 @@ VALUES
 ('Strength Basics', 3);
 
 CREATE TABLE Class_Schedule (
-    Class_ID INT NOT NULL,
-    Day VARCHAR(20) NOT NULL CHECK (Day IN 
-    ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')),
-    Time TIME NOT NULL,
-    PRIMARY KEY (Class_ID, Day, Time),
-    FOREIGN KEY (Class_ID) REFERENCES Class(ID)
+    Class_ID INT NOT NULL,
+    Day VARCHAR(20) NOT NULL CHECK (Day IN 
+    ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')),
+    Time TIME NOT NULL,
+    PRIMARY KEY (Class_ID, Day, Time),
+    FOREIGN KEY (Class_ID) REFERENCES Class(ID)
 );
 
 INSERT INTO Class_Schedule VALUES
@@ -149,22 +169,23 @@ CREATE TABLE Equipment (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     Name VARCHAR(50) NOT NULL,
     Purchase_Date DATE,
-    Maintainance_Duration INT CHECK (Maintainance_Duration > 0)
+    Maintainance_Duration INT CHECK (Maintainance_Duration > 0),
+    Branch_ID INT NOT NULL, 
+    FOREIGN KEY (Branch_ID) REFERENCES Branch_Manager(Branch_ID) 
 );
 
-INSERT INTO Equipment
-(Name, Purchase_Date, Maintainance_Duration)
+INSERT INTO Equipment (Name, Purchase_Date, Maintainance_Duration, Branch_ID)
 VALUES
-('Treadmill', '2024-01-10', 90),
-('Yoga Mat', '2024-03-15', 180),
-('Barbell Set', '2023-11-20', 120);
+('Treadmill', '2024-01-10', 90, 1),
+('Yoga Mat', '2024-03-15', 180, 2),
+('Barbell Set', '2023-11-20', 120, 3);
 
 CREATE TABLE Offers (
-    Branch_ID INT NOT NULL,
-    Class_ID INT NOT NULL,
-    PRIMARY KEY (Branch_ID, Class_ID),
-    FOREIGN KEY (Branch_ID) REFERENCES Branch_Manager(Branch_ID),
-    FOREIGN KEY (Class_ID) REFERENCES Class(ID)
+    Branch_ID INT NOT NULL,
+    Class_ID INT NOT NULL,
+    PRIMARY KEY (Branch_ID, Class_ID),
+    FOREIGN KEY (Branch_ID) REFERENCES Branch_Manager(Branch_ID),
+    FOREIGN KEY (Class_ID) REFERENCES Class(ID)
 );
 
 INSERT INTO Offers VALUES
